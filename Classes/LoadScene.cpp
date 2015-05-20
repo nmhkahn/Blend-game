76ecefd0a6eb3ec03625f8d4457363da78e2acb0
nmhkahn <nmhkahn@gmail.com>
2015-05-21 01:50:22 +0900

    modify resources

---
+++ Classes/LoadScene.cpp
@@ -0,0 +1,9 @@
+//
+//  LoadScene.cpp
+//  Blend
+//
+//  Created by An on 5/21/15.
+//
+//
+
+#include "LoadScene.h"
---
+++ Classes/LoadScene.h
@@ -0,0 +1,14 @@
+//
+//  LoadScene.h
+//  Blend
+//
+//  Created by An on 5/21/15.
+//
+//
+
+#ifndef __Blend__LoadScene__
+#define __Blend__LoadScene__
+
+#include <stdio.h>
+
+#endif /* defined(__Blend__LoadScene__) */
