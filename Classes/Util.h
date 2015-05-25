a39ef7f1eba17122a0e2f031debe7ec31d253bba
nmhkahn <nmhkahn@gmail.com>
2015-05-25 23:55:44 +0900

    add constant flow_speed

--- Classes/Util.h
+++ Classes/Util.h
@@ -6,17 +6,15 @@ USING_NS_CC;
 
 enum TYPE {NODE = 0, N_PIPE = 10, R_PIPE = 20, S_PIPE = 30, T_PIPE = 40};
 enum COLOR {RED, BLUE, GREEN, YELLOW, MAGENTA, ORANGE};
+enum COND {NONE, WIN, L_BLEND, L_NOCONN, L_TOTAL};
 enum DIR {UP, DOWN, RIGHT, LEFT};
 
 constexpr const int num_grid_width = 14;
 constexpr const int num_grid_height = 8;
 
-constexpr const int max_stage = 4;
+constexpr const int max_level = 5;
 
-constexpr const int lose_blend = 1;
-constexpr const int lose_noconnect = 2;
-constexpr const int lose_total = 3;
-constexpr const int win = 4;
+constexpr const float flow_speed = 0.1;
 
 /* util function */
 Vec2 gridToPos( const Vec2& grid );
